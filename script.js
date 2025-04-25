51cfbc74226c166955d62b6dd27254947a4e8241
Edwige08 <edwige.saves@gmail.com>
2025-04-25 11:18:09 +0200

    add: function creatingProfileCards() to simplify the creation of the profile cards

--- script.js
+++ script.js
@@ -13,52 +13,54 @@ const searchCity = document.querySelector('#search-city');
 const amountOfMoments = document.querySelector('#amount-of-moments')
 
 // LES FONCTIONS : ------------------------------------------------------
+const creatingProfileCards = (profiles) => {
+    const profileArticle = document.createElement('article');
+    const profileImgDiv = document.createElement('div');
+    const profileImg = document.createElement('img');
+    const profileDetailsDiv = document.createElement('div');
+    const profileMoment = document.createElement('p');
+    const profileName = document.createElement('p');
+    const profileJobAndAge = document.createElement('p');
+    const profileLocalisation = document.createElement('p');
+    const profileDescription = document.createElement('p');
+    const profileButton = document.createElement('button');
 
+    profileArticle.classList.add('profile-article');
+    profileImgDiv.classList.add('profile-img-div');
+    profileImg.classList.add('profile-img');
+    profileDetailsDiv.classList.add('profile-details-div')
+    profileMoment.classList.add('profile-moment');
+    profileName.classList.add('profile-name');
+    profileJobAndAge.classList.add('profile-job-age');
+    profileLocalisation.classList.add('profile-city');
+    profileDescription.classList.add('profile-description');
+    profileButton.classList.add('btn-primary');
+
+    profileImg.src = `${profiles.imageUrl}`
+    profileMoment.innerText = profiles.type;
+    profileName.innerText = profiles.firstname;
+    profileJobAndAge.innerText = `${profiles.job} ⸱ ${profiles.age} ans`;
+    profileLocalisation.innerText = `${profiles.city} (${profiles.zipcode})`;
+    profileDescription.innerText = profiles.description;
+    profileButton.innerText = 'Programmer un moment'
+
+    profileImgDiv.appendChild(profileImg)
+    profileArticle.appendChild(profileImgDiv);
+    profileDetailsDiv.appendChild(profileMoment);
+    profileDetailsDiv.appendChild(profileName);
+    profileDetailsDiv.appendChild(profileJobAndAge);
+    profileDetailsDiv.appendChild(profileLocalisation);
+    profileDetailsDiv.appendChild(profileDescription);
+    profileArticle.appendChild(profileDetailsDiv);
+    profileArticle.appendChild(profileButton);
+    searchResults.appendChild(profileArticle);
+}
 const displayProfiles = (listOfProfiles) => {
     searchResults.innerText = '';
     let numberOfResults = 0;
     for (let x = 0; x < listOfProfiles.length; x++) {
         numberOfResults++;
-        const profileArticle = document.createElement('article');
-        const profileImgDiv = document.createElement('div');
-        const profileImg = document.createElement('img');
-        const profileDetailsDiv = document.createElement('div');
-        const profileMoment = document.createElement('p');
-        const profileName = document.createElement('p');
-        const profileJobAndAge = document.createElement('p');
-        const profileLocalisation = document.createElement('p');
-        const profileDescription = document.createElement('p');
-        const profileButton = document.createElement('button');
-
-        profileArticle.classList.add('profile-article');
-        profileImgDiv.classList.add('profile-img-div');
-        profileImg.classList.add('profile-img');
-        profileDetailsDiv.classList.add('profile-details-div')
-        profileMoment.classList.add('profile-moment');
-        profileName.classList.add('profile-name');
-        profileJobAndAge.classList.add('profile-job-age');
-        profileLocalisation.classList.add('profile-city');
-        profileDescription.classList.add('profile-description');
-        profileButton.classList.add('btn-primary');
-
-        profileImg.src = `${listOfProfiles[x].imageUrl}`
-        profileMoment.innerText = listOfProfiles[x].type;
-        profileName.innerText = listOfProfiles[x].firstname;
-        profileJobAndAge.innerText = `${listOfProfiles[x].job} ⸱ ${listOfProfiles[x].age} ans`;
-        profileLocalisation.innerText = `${listOfProfiles[x].city} (${listOfProfiles[x].zipcode})`;
-        profileDescription.innerText = listOfProfiles[x].description;
-        profileButton.innerText = 'Programmer un moment'
-
-        profileImgDiv.appendChild(profileImg)
-        profileArticle.appendChild(profileImgDiv);
-        profileDetailsDiv.appendChild(profileMoment);
-        profileDetailsDiv.appendChild(profileName);
-        profileDetailsDiv.appendChild(profileJobAndAge);
-        profileDetailsDiv.appendChild(profileLocalisation);
-        profileDetailsDiv.appendChild(profileDescription);
-        profileArticle.appendChild(profileDetailsDiv);
-        profileArticle.appendChild(profileButton);
-        searchResults.appendChild(profileArticle);
+        creatingProfileCards(listOfProfiles[x]);
     }
     if (numberOfResults < 2) {
         amountOfMoments.innerText = `${numberOfResults} moment trouvé`
@@ -74,46 +76,13 @@ const displayProfilesWithFilters = (listOfProfiles, activity, city) => {
         if (listOfProfiles[x].type === activity) {
             if (listOfProfiles[x].city.toLowerCase() === city.toLowerCase()) {
                 numberOfResults++;
-                const profileArticle = document.createElement('article');
-                const profileImgDiv = document.createElement('div');
-                const profileImg = document.createElement('img');
-                const profileDetailsDiv = document.createElement('div');
-                const profileMoment = document.createElement('p');
-                const profileName = document.createElement('p');
-                const profileJobAndAge = document.createElement('p');
-                const profileLocalisation = document.createElement('p');
-                const profileDescription = document.createElement('p');
-                const profileButton = document.createElement('button');
-
-                profileArticle.classList.add('profile-article');
-                profileImgDiv.classList.add('profile-img-div');
-                profileImg.classList.add('profile-img');
-                profileDetailsDiv.classList.add('profile-details-div')
-                profileMoment.classList.add('profile-moment');
-                profileName.classList.add('profile-name');
-                profileJobAndAge.classList.add('profile-job-age');
-                profileLocalisation.classList.add('profile-city');
-                profileDescription.classList.add('profile-description');
-                profileButton.classList.add('btn-primary');
-
-                profileImg.src = `${listOfProfiles[x].imageUrl}`
-                profileMoment.innerText = listOfProfiles[x].type;
-                profileName.innerText = listOfProfiles[x].firstname;
-                profileJobAndAge.innerText = `${listOfProfiles[x].job} ⸱ ${listOfProfiles[x].age} ans`;
-                profileLocalisation.innerText = `${listOfProfiles[x].city} (${listOfProfiles[x].zipcode})`;
-                profileDescription.innerText = listOfProfiles[x].description;
-                profileButton.innerText = 'Programmer un moment'
-
-                profileImgDiv.appendChild(profileImg)
-                profileArticle.appendChild(profileImgDiv);
-                profileDetailsDiv.appendChild(profileMoment);
-                profileDetailsDiv.appendChild(profileName);
-                profileDetailsDiv.appendChild(profileJobAndAge);
-                profileDetailsDiv.appendChild(profileLocalisation);
-                profileDetailsDiv.appendChild(profileDescription);
-                profileArticle.appendChild(profileDetailsDiv);
-                profileArticle.appendChild(profileButton);
-                searchResults.appendChild(profileArticle);
+                creatingProfileCards(listOfProfiles[x]);
+            }
+        } 
+        if (moments.value === 'all-moments') {
+            if (listOfProfiles[x].city.toLowerCase() === city.toLowerCase()) {
+                numberOfResults++;
+                creatingProfileCards(listOfProfiles[x]);
             }
         }
     }
@@ -135,10 +104,5 @@ displayProfiles(allProfiles);
 
 // LES EVENT LISTENERS : ------------------------------------------------
 searchMoment.addEventListener('click', () => {
-    console.log('hello')
-    if (moments.value === 'all-moments') {
-        displayProfiles(allProfiles);
-    } else {
         displayProfilesWithFilters(allProfiles, moments.value, searchCity.value);
-    }
 })
